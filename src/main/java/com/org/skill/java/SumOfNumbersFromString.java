cb3659c4e0a8f5791065dca2b199267bf50a7d3f
Ravindra Kumar <ravindra.kumar@netcracker.com>
2024-12-29 07:53:24 +0530

    SumOfNumbersFromString modified

--- src/main/java/com/org/skill/java/SumOfNumbersFromString.java
+++ src/main/java/com/org/skill/java/SumOfNumbersFromString.java
@@ -1,8 +1,18 @@
 package com.org.skill.java;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class SumOfNumbersFromString {
+
+    public static int sumOfNumbersFromStringV2(String text) {
+        if(text == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        return Arrays.asList(text.split(" ")).stream().
+                filter((value) -> value.matches("\\d+")).
+                collect(Collectors.summingInt(Integer::valueOf));
+    }
 
     public static int sumOfNumbersFromString(String text) {
         if(text == null) {
--- src/test/java/com/org/skill/java/SumOfNumbersFromStringTest.java
+++ src/test/java/com/org/skill/java/SumOfNumbersFromStringTest.java
@@ -7,6 +7,16 @@ import static org.junit.Assert.*;
 public class SumOfNumbersFromStringTest {
 
     @Test
+    public void testSumfOfNumbersFromStringV2NotSame() {
+        assertNotEquals(10, SumOfNumbersFromString.sumOfNumbersFromStringV2("Item1 3, item2 7"));
+    }
+
+    @Test
+    public void testSumOfNumbersFromStringV2() {
+        assertEquals(10, SumOfNumbersFromString.sumOfNumbersFromStringV2("Item1 3 Item2 5 Item3 2"));
+    }
+
+    @Test
     public void testSumOfNumbersFromString() {
         assertEquals(5, SumOfNumbersFromString.sumOfNumbersFromString("order1 4 order2 1"));
     }
